d5b04910e32beaf647377082d4f8f5323f6b2828
tomhartgers <95407658+tomhartgers@users.noreply.github.com>
2021-12-06 16:24:04 +0100

    Update data_import.R

--- data_import.R
+++ data_import.R
@@ -2,6 +2,7 @@
 # setup----
 if (!require(tidyverse)) install.packages("tidyverse") else library(tidyverse)
 library(ggplot2)
+library(magrittr)
 
 # The file which is loaded below still includes all variables.
 # Game titles are normalized, top 10 games ranked, rest grouped as "Other".
